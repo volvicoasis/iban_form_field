ffa8ea61987fdefeaa8295fe4529b13ee3d2cd94
Jacques Leceux <jleceux@opensense.fr>
2024-08-22 10:36:33 +0200

    bodyMedium - replace bodyText2

--- lib/src/iban_form_field.dart
+++ lib/src/iban_form_field.dart
@@ -159,7 +159,7 @@ class _IbanFormFieldState extends State<IbanFormFieldBuilder> {
   @override
   Widget build(BuildContext context) {
     var constraints = BoxConstraints.tightFor(
-      width: Theme.of(widget.state.context).textTheme.bodyText2!.fontSize! *
+      width: Theme.of(widget.state.context).textTheme.bodyMedium!.fontSize! *
           1.1 *
           2,
     );
